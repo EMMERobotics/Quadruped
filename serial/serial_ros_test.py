63d9009e3adfd6da3121000f1dc2d4fd7984dc3c
tyuuio25 <sitthichok.c@ku.th>
2022-01-02 22:04:48 +0700

    add serial for jetson

--- serial/serial_ros_test.py
+++ serial/serial_ros_test.py
@@ -10,44 +10,18 @@ serial_port = serial.Serial(
     bytesize=serial.EIGHTBITS,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
-    
+    )
 time.sleep(1)
     
-try
-    # Send a simple header
-    while True:
-        #if serial_port.inWaiting() > 0:
-            #serial_port.open()
-            #print(serial_port.is_open)
-            drive  = "#1P2000T1000\r\n".encode()
-
-            serial_port.write(drive)
-            time.sleep(5)
-
-            stop = "#1P1500T1000\r\n".encode()
-            serial_port.write(stop)
-            time.sleep(5)
-            # if we get a carriage return, add a line feed too
-            # \r is a carriage return; \n is a line feed
-            # This is to help the tty program on the other end 
-            # Windows is \r\n for carriage return, line feed
-            # Macintosh and Linux use \n
-                # For Windows boxen on the other end
-
-
-except KeyboardInterrupt:
-    print("Exiting Program")
-
-except Exception as exception_error:
-    print("Error occurred. Exiting Program")
-    print("Error: " + str(exception_error))
-
-finally:
-    serial_port.close()
-    pass
+#finally:
+    #serial_port.close()
+    #pass
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    serial_port.write(data.data.encode())
+
+  
 
 def listener():
 
@@ -59,3 +33,4 @@ def listener():
 
 if __name__ == '__main__':
     listener()
+
