a63aa3b6790fc4de74e77d5c29b41ad472c3c34d
EarthYim <45221890+EarthYim@users.noreply.github.com>
2022-09-29 13:09:07 +0700

    Create controller.py

---
+++ controller/controller.py
@@ -0,0 +1,35 @@
+import evdev
+import ev3dev.auto as ev3
+import threading
+
+#creates object 'gamepad' to store the data
+gamepad = evdev.InputDevice('/dev/input/event0')  # <====== change this to correct addr
+print("controller established")
+
+## Some helpers ##
+def scale(val, src, dst):
+    """
+    Scale the given value from the scale of src to the scale of dst.
+
+    val: float or int
+    src: tuple
+    dst: tuple
+
+    example: print(scale(99, (0.0, 99.0), (-1.0, +1.0)))
+    """
+    return (float(val - src[0]) / (src[1] - src[0])) * (dst[1] - dst[0]) + dst[0]
+
+def scale_stick(value):
+    return scale(value,(0,255),(-100,100))
+
+
+for event in gamepad.read_loop():   #this loops infinitely
+    if event.type == 3:             #A stick is moved
+        if event.code == 5:         #Y axis on right stick
+            #do something
+            abc = 0
+        
+    if event.type == 1 and event.code == 302 and event.value == 1:
+        print("X button is pressed. Stopping.")
+        running = False
+        break
