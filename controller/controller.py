6c73b3287b820aee20585083e5d6f20f61f042f6
EarthYim <45221890+EarthYim@users.noreply.github.com>
2022-10-03 16:47:19 +0700

    add com_msg and read val

--- controller/controller.py
+++ controller/controller.py
@@ -1,36 +1,58 @@
 #!/usr/bin/env python3
 import evdev
 import ev3dev.auto as ev3
-import threading
+import rospy
+from quadruped_main.msg import con_msg
 
-#creates object 'gamepad' to store the data
-gamepad = evdev.InputDevice('/dev/input/event0')  # <====== change this to correct addr
-print("controller established")
 
 ## Some helpers ##
+def scale_stick(value):
+    return scale(value,(0,255),(-100,100))
+
 def scale(val, src, dst):
     """
     Scale the given value from the scale of src to the scale of dst.
-
     val: float or int
     src: tuple
     dst: tuple
-
     example: print(scale(99, (0.0, 99.0), (-1.0, +1.0)))
     """
     return (float(val - src[0]) / (src[1] - src[0])) * (dst[1] - dst[0]) + dst[0]
 
-def scale_stick(value):
-    return scale(value,(0,255),(-100,100))
+
+#creates object 'gamepad' to store the data
+gamepad = evdev.InputDevice('/dev/input/event1')  # <====== change this to correct addr
+print("controller established")
 
 
-for event in gamepad.read_loop():   #this loops infinitely
-    if event.type == 3:             #A stick is moved
-        if event.code == 5:         #Y axis on right stick
-            #do something
-            abc = 0
+def talker():
+    pub = rospy.Publisher('joy_val', con_msg, queue_size=10)
+    rospy.init_node('joystick', anonymous=True)
+    rate = rospy.Rate(100) # 10hz
+    
+    while not rospy.is_shutdown():
+        for event in gamepad.read_loop():   #this loops infinitely
+            if event.type == 3:             #A stick is moved
+                if event.code == 5:         #Y axis on right stick
+                    con_msg.val_y2 = scale_stick(event.value)
+                if event.code == 2:         #X axis on right stick
+                    con_msg.val_x2 = scale_stick(event.value)
+                if event.code == 0:         #X axis on left stick
+                    con_msg.val_xl = scale_stick(event.value)
+                if event.code == 1:         #Y axis on left stick
+                    con_msg.val_y1 = scale_stick(event.value)
         
-    if event.type == 1 and event.code == 302 and event.value == 1:
-        print("X button is pressed. Stopping.")
-        running = False
-        break
+        rospy.loginfo(con_msg)
+        pub.publish(con_msg)
+        rate.sleep()
+           
+   
+    if __name__ == '__main__':
+       try:
+           talker()
+       except rospy.ROSInterruptException:
+           pass
+
+
+
+
