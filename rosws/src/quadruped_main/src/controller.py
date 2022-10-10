adf9797654c65e9e7297d41a90040b77e9023dc9
EarthYim <45221890+EarthYim@users.noreply.github.com>
2022-10-10 17:27:19 +0700

    add value filtering to controller.py

--- rosws/src/quadruped_main/src/controller.py
+++ rosws/src/quadruped_main/src/controller.py
@@ -18,28 +18,42 @@ def scale(val, src, dst):
     """
     return (float(val - src[0]) / (src[1] - src[0])) * (dst[1] - dst[0]) + dst[0]
 
-
 #creates object 'gamepad' to store the data
 gamepad = evdev.InputDevice('/dev/input/event1')  # <====== change this to correct addr
 rospy.loginfo("controller established")
 msg = con_msg()
+
+
+def deadzone(val, center, range):
+    if val > (center-range) and val < (center+range):
+        return center
+
+    elif val < 0 or val > 256:
+        rospy.loginfo("Controller Value out of range")
+        return center
+
+    else:
+        return val
+
+
 def joystick():
     pub = rospy.Publisher('joy_val', con_msg, queue_size=10)
     rospy.init_node('joystick', anonymous=True)
     rate = rospy.Rate(100) # 10hz
+
     while not rospy.is_shutdown():
-        event = gamepad.read_one()   #this loops infinitely
-        
+        #read value from controller
+        event = gamepad.read_one()
         if event != None:
             if event.type == 3:             #A stick is moved
                 if event.code == 4:         #Y axis on right stick
-                    msg.val_y2 = event.value
+                    msg.val_y2 = deadzone(event.value, 127, 16)
                 if event.code == 3:         #X axis on right stick
-                    msg.val_x2 = event.value
+                    msg.val_x2 = deadzone(event.value, 127, 16)
                 if event.code == 0:         #X axis on left stick
-                    msg.val_x1 = event.value
+                    msg.val_x1 = deadzone(event.value, 127, 16)
                 if event.code == 1:         #Y axis on left stick
-                    msg.val_y1 = event.value
+                    msg.val_y1 = deadzone(event.value, 127, 16)
 
         #rospy.loginfo(con_msg.val_y2)
         pub.publish(msg)
